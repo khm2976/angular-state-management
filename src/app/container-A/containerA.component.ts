651122f178013f4fd873d14fcc2f5db0dddab32e
hyemikim-29cm <khm@29cm.co.kr>
2020-07-29 01:25:45 +0900

    feat: changeDetection onPush 추가(#master)

--- src/app/container-A/containerA.component.ts
+++ src/app/container-A/containerA.component.ts
@@ -1,40 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Category } from '../shared/state.model';
-import { StoreService } from '../shared/store.service';
 
 @Component({
     selector: 'app-container-a',
     templateUrl: './containerA.component.html',
     styleUrls: ['./containerA.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContainerAComponent implements OnInit {
     @Input() category1: Category;
     @Input() category2: Category;
 
     constructor(
-        private http: HttpClient,
-        private store: StoreService,
     ) {}
 
     ngOnInit() {
-    }
-
-    getCate1() {
-        this.http
-            .get('/assets/mock/category1.json')
-            .subscribe(res => {
-                //this.cate1 = res as Category;
-                //console.log(this.cate1);
-            });
-    }
-
-    getCate2() {
-        this.http
-            .get('/assets/mock/category2.json')
-            .subscribe(res => {
-                //this.cate2 = res as Category;
-            });
     }
 }
--- src/app/container-B/containerB.component.ts
+++ src/app/container-B/containerB.component.ts
@@ -1,31 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Product } from '../shared/state.model';
-import { StoreService } from '../shared/store.service';
-import { SelectorService } from '../shared/selector.service';
 
 @Component({
     selector: 'app-container-b',
     templateUrl: './containerB.component.html',
     styleUrls: ['./containerB.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContainerBComponent implements OnInit {
     @Input() product: Product;
 
     constructor(
-        private http: HttpClient,
     ) {}
 
     ngOnInit() {
-        this.getProduct();
-    }
-
-    getProduct() {
-        this.http
-            .get('/assets/mock/detail.json')
-            .subscribe(res => {
-            //    this.product = res as Product;
-            });
     }
 }
